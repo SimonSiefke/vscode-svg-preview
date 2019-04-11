67ca22d920a86a5c14fe5fa809a5bd1a85869782
s.siefke.ext@bitworkers.eu <simon.siefke@gmail.com>
2019-04-11 21:49:22 +0200

    fix: don't autoopen when a tab was closed

--- packages/extension/src/extension.ts
+++ packages/extension/src/extension.ts
@@ -12,6 +12,10 @@ export let context: vscode.ExtensionContext
  */
 export async function activate(c: vscode.ExtensionContext): Promise<void> {
   context = c
+  /**
+   * Whether or not the last text editor event was a close event or not
+   */
+  let lastEventWasClose = false
   context.subscriptions.push(
     vscode.commands.registerTextEditorCommand(
       'svgPreview.showPreview',
@@ -44,32 +48,55 @@ export async function activate(c: vscode.ExtensionContext): Promise<void> {
   )
   context.subscriptions.push(
     vscode.window.onDidChangeActiveTextEditor(textEditor => {
+      // there is nothing to open
+      if (!textEditor) {
+        return
+      }
+      // don't open when a tab was closed
+      if (lastEventWasClose) {
+        lastEventWasClose = false
+        return
+      }
+      // don't open if it's not an svg
+      if (!shouldOpenTextDocument(textEditor.document, previewPanel.fsPath)) {
+        return
+      }
+      // don't open if auto-open setting isn't enabled
       if (
-        !textEditor ||
-        (!previewPanel.fsPath &&
-          !configuration.get('autoOpen', textEditor.document.uri))
+        !previewPanel.fsPath &&
+        !configuration.get('autoOpen', textEditor.document.uri)
       ) {
         return
       }
-      const textDocument = textEditor.document
-      if (shouldOpenTextDocument(textDocument, previewPanel.fsPath)) {
-        previewPanel.show({
-          viewColumn: vscode.ViewColumn.Beside,
-          fsPath: textDocument.uri.fsPath,
-        })
-        previewPanel.content = textDocument.getText()
-      }
+      // open the preview
+      previewPanel.show({
+        viewColumn: vscode.ViewColumn.Beside,
+        fsPath: textEditor.document.uri.fsPath,
+      })
+      previewPanel.content = textEditor.document.getText()
     })
   )
   if (DEVELOPMENT) {
     // This is still proposed api so it cannot be used in production at the moment
-    vscode.workspace.onDidRenameFile(event => {
-      if (previewPanel.fsPath !== event.oldUri.fsPath) {
-        return
-      }
-      previewPanel.fsPath = event.newUri.fsPath
-    })
+    context.subscriptions.push(
+      vscode.workspace.onDidRenameFile(event => {
+        if (previewPanel.fsPath !== event.oldUri.fsPath) {
+          return
+        }
+        previewPanel.fsPath = event.newUri.fsPath
+      })
+    )
   }
+  context.subscriptions.push(
+    vscode.workspace.onDidOpenTextDocument(() => {
+      lastEventWasClose = false
+    })
+  )
+  context.subscriptions.push(
+    vscode.workspace.onDidCloseTextDocument(() => {
+      lastEventWasClose = true
+    })
+  )
   context.subscriptions.push(
     vscode.workspace.onDidChangeTextDocument(event => {
       const shouldUpdateTextDocument =
